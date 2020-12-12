8ed5eb550916709a7add0d87ccf18876b8b6dd24
phungnm99-it <62021483+phungnm99-it@users.noreply.github.com>
2020-12-12 20:43:50 +0700

    add-validator

---
+++ PTStore/Models/Validator/AccountValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PTStore.Models.Validator
+{
+	public class AccountValidator : AbstractValidator<Account>
+	{
+		public AccountValidator()
+		{
+			//RuleFor(x => x.TenDangNhap).NotNull();
+			RuleFor(x => x.TenDangNhap).NotEmpty().WithMessage("Tên đăng nhập không được để trống!");
+			RuleFor(x => x.MatKhau).Length(8, 255).WithMessage("Mật khẩu phải lớn hơn 8 kí tự!");
+		}
+	}
+}
